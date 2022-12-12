06339a611be3a76c2ca673c11ed13a851035a371
amar0020 <amarkrsoni8989@gmail.com>
2022-12-12 14:16:13 +0530

    amar

--- src/Pages/user.jsx
+++ src/Pages/user.jsx
@@ -24,6 +24,7 @@ export const User = ()=>{
       })
         
         .then((r)=>{
+          console.log(r)
           localStorage.setItem('token',r.token)
           navigate("/notice")
         })
